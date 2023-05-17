666d9d2a7d14ed75502f202374ff486bcc130fa1
Leonardo Saucedo <l.olmossaucedo@gmail.com>
2023-05-16 19:40:30 -0600

    [FIX] Replace wrong file

--- sql_lab_join_multiple.sql
+++ sql_lab_join_multiple.sql
@@ -1,39 +1,83 @@
--- Leonardo Olmos Saucedo / SQL Self and Cross join lab
+-- Leonardo Olmos Saucedo / Lab SQL Joins
 
--- 1. Get all pairs of actors that worked together.
-select distinct F.TITLE, FA.ACTOR_ID, A.FIRST_NAME, FA2.ACTOR_ID, A2.FIRST_NAME
-from FILM_ACTOR FA 
-join FILM_ACTOR FA2 
-on FA.FILM_ID  = FA2.FILM_ID 
-join ACTOR A 
-on A.ACTOR_ID = FA.ACTOR_ID 
-join ACTOR A2
-on A2.ACTOR_ID = FA2.ACTOR_ID 
-join FILM F  
-on FA.FILM_ID = F.FILM_ID 
-where FA.ACTOR_ID < FA2.ACTOR_ID
-order by 1, 2, 4;
+-- 1. Write a query to display for each store its store ID, city, and country.
+select S.STORE_ID, C.CITY, C2.COUNTRY 
+from STORE S 
+join ADDRESS A 
+on A.ADDRESS_ID = S.ADDRESS_ID 
+join CITY C 
+on C.CITY_ID = A.CITY_ID 
+join COUNTRY C2 
+on C2.COUNTRY_ID = C.COUNTRY_ID;
 
--- 2. Get all pairs of customers that have rented the same film more than 3 times.
-select DISTINCT R.CUSTOMER_ID, R2.CUSTOMER_ID, COUNT(*) as COINCIDENCES
-from RENTAL R 
+-- 2. Write a query to display how much business, in dollars, each store brought in.
+select S.STORE_ID, SUM(P.AMOUNT) as TOTAL_AMOUNT
+from STORE S 
 join INVENTORY I 
-on R.INVENTORY_ID = I.INVENTORY_ID 
-join FILM F 
-on I.FILM_ID = F.FILM_ID 
-join FILM F2 
-on F.FILM_ID = F2.FILM_ID 
-join INVENTORY I2 
-on F2.FILM_ID = I2.FILM_ID 
-join RENTAL R2 
-on R2.INVENTORY_ID = I2.INVENTORY_ID 
-where R.CUSTOMER_ID <> R2.CUSTOMER_ID 
-group by R.CUSTOMER_ID , R2.CUSTOMER_ID 
-having COUNT(*) > 3
-order by 1, 2 ;
+on I.STORE_ID = S.STORE_ID 
+join RENTAL R 
+on I.INVENTORY_ID = R.INVENTORY_ID 
+join PAYMENT P 
+on P.RENTAL_ID = R.RENTAL_ID 
+group by S.STORE_ID;
 
--- 3. Get all possible pairs of actors and films.
-select A.ACTOR_ID, A.FIRST_NAME, F.TITLE 
-from ACTOR A 
-cross join FILM F 
-order by 1;
+-- 3. What is the average running time of films by category?
+select C.NAME, AVG(F.`length`) as AVG_LENGTH
+from FILM F 
+join FILM_CATEGORY FC 
+on FC.FILM_ID = F.FILM_ID 
+join CATEGORY C 
+on C.CATEGORY_ID = FC.CATEGORY_ID 
+group by C.NAME;
+
+-- 4. Which film categories are longest?
+select C.NAME, AVG(F.`length`) as AVG_LENGTH
+from FILM F 
+join FILM_CATEGORY FC 
+on FC.FILM_ID = F.FILM_ID 
+join CATEGORY C 
+on C.CATEGORY_ID = FC.CATEGORY_ID 
+group by C.NAME
+order by 2 desc 
+limit 1;
+
+-- 5. Display the most frequently rented movies in descending order.
+select F.TITLE, COUNT(R.INVENTORY_ID) as RENTAL_FREQ
+from FILM F 
+join INVENTORY I 
+on I.FILM_ID = F.FILM_ID 
+join RENTAL R 
+on R.INVENTORY_ID = I.INVENTORY_ID
+group by F.TITLE 
+order by 2 desc;
+
+-- 6. List the top five genres in gross revenue in descending order.
+select C.NAME, SUM(P.AMOUNT) as 'GROSS_REVENUE'
+from CATEGORY C 
+join FILM_CATEGORY FC 
+on C.CATEGORY_ID = FC.CATEGORY_ID 
+join FILM F 
+on FC.FILM_ID = F.FILM_ID 
+join INVENTORY I 
+on F.FILM_ID = I.FILM_ID 
+join RENTAL R 
+on I.INVENTORY_ID = R.INVENTORY_ID 
+join PAYMENT P 
+on R.RENTAL_ID = P.RENTAL_ID 
+group by C.NAME 
+order by 2 desc 
+limit 5;
+
+-- 7. Is "Academy Dinosaur" available for rent from Store 1?
+select *
+from FILM F 
+join INVENTORY I 
+on F.FILM_ID = I.FILM_ID 
+left join RENTAL R 
+on I.INVENTORY_ID = R.INVENTORY_ID 
+where F.TITLE = 'Academy Dinosaur'
+and R.RENTAL_ID is NULL 
+and I.STORE_ID = 1;
+
+-- NO, THERE IS NO AVAILABLE FOR RENT IN STORE 2
+
